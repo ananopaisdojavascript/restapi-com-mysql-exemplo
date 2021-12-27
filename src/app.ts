27c714b6e7d1e102ad55177d175e6e273b35c3ef
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2021-12-27 17:59:56 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -62,16 +62,6 @@ function deleteName(request: Request, response: Response) {
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    //
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = 'client/build/index.html'));
-    })
-}
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/public/index.html'));
 })
