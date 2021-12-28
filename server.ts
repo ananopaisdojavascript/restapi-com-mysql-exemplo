bd5d65a867402e6e817c1ea85d377c2ff90d2143
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2021-12-28 18:54:00 -0300

    Ajustes

--- server.ts
+++ server.ts
@@ -1,27 +1,69 @@
 import express, { Request, Response } from 'express';
 import mysql2 from 'mysql2';
+import bodyParser from 'body-parser';
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-const connection = mysql2.createConnection({ 
-	host: process.env.DATABASE_HOST, 
-	user: process.env.DATABASE_USER, 
-	password: process.env.DATABASE_PASSWORD, 
-	database: process.env.DATABASE_DB 
+const connection = mysql2.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB
 });
 
-connection.connect(function (err) { 
-if (err) throw err; 
-console.log("Até que enfim conectou!!!!!"); 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Até que enfim conectou!!!!!");
 });
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
 
 app.get('/members', (request: Request, response: Response) => {
     const query = 'SELECT * FROM members';
     connection.query(query, (error, results, fields) => {
-        if(error) throw error;
+        if (error) throw error;
         response.send(JSON.stringify(results));
     })
 })
+
+app.get('/members/:id', (request: Request, response: Response) => {
+    const id = request.params.id;
+    const query = `SELECT * FROM members WHERE ID = ${id}`;
+    connection.query(query, (error, results, fields) => {
+        if (error) throw error;
+        response.send(JSON.stringify(results));
+    })
+})
+
+app.post('/members', (request: Request, response: Response) => {
+    const params = request.body;
+    const query = `INSERT INTO members SET ?  `;
+    connection.query(query, params, function (error, results, fields) {
+        if (error) throw error;
+        response.end(JSON.stringify(results));
+    });
+})
+
+app.put('/members/:id', (request: Request, response: Response) => {
+    const id = request.params.id;
+    const query = 'UPDATE `members` SET `name` = ?, `group` = ? WHERE ID = ' + id;
+    connection.query(query, [request.body.name, request.body.group, id], function (error, results, fields) {
+        if (error) throw error;
+        response.end(JSON.stringify(results));
+    });
+})
+
+app.delete('/members/:id',(request: Request, response: Response) => {
+    const id = request.body.id;
+    const query = 'DELETE from `members` WHERE `Id` = ?';
+    connection.query(query, id, (error, results, fields) => {
+        if (error) throw error;
+        response.send('Record has been deleted!')
+    })
+})
+
 
 app.listen(port, () => console.log(`Está funcionando na porta ${port}!! Uhu!!!`))
